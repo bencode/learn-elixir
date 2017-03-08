cef590ab25073e35c1305e0ed1d349336fbf95c6
bencode <bencode@163.com>
2017-03-08 11:27:33 +0800

    add plug demo

---
+++ hello/lib/server.ex
@@ -0,0 +1,16 @@
+defmodule Server do
+  import Plug.Conn
+
+
+  def init(opts) do
+    opts
+  end
+
+
+  def call(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "Hello world")
+  end
+end
+
