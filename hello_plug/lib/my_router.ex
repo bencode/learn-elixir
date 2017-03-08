d32e7dd7ae86c5b3a735004d313342d2b8151101
bencode <bencode@163.com>
2017-03-08 23:51:39 +0800

    code for parser

--- hello_plug/lib/my_router.ex
+++ hello_plug/lib/my_router.ex
@@ -1,15 +1,34 @@
 defmodule MyRouter do
   use Plug.Router
+
   plug Plug.Logger
   plug :match
+  plug Plug.Parsers, parsers: [:urlencoded, :json],
+                     pass: ["application/x-www-form-urlencoded", "application/json"],
+                     json_decoder: Poison
   plug :dispatch
 
 
   get "/hello" do
-    IO.puts "hello"
     conn
     |> put_resp_content_type("text/plain")
     |> send_resp(200, "helo world")
+  end
+
+
+  get "/products/:name" do
+    send_resp(conn, 200, "products #{name}")
+  end
+
+
+  get "/my/*glob" do
+    send_resp(conn, 200, "router after: #{glob}")
+  end
+
+
+  post "/products" do
+    IO.inspect conn.body_params
+    conn |> send_resp(200, "success!")
   end
 
 
--- hello_plug/mix.exs
+++ hello_plug/mix.exs
@@ -27,7 +27,10 @@ defmodule HelloPlug.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:cowboy, "~> 1.0"},
-    {:plug, "~> 1.0"}]
+    [
+      {:poison, "~> 3.0"},
+      {:cowboy, "~> 1.0"},
+      {:plug, "~> 1.0"}
+    ]
   end
 end
