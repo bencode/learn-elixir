c2d2b792f2232709d133675adbc6f175453edbc2
bencode <bencode@163.com>
2017-03-12 22:41:14 +0800

    update

--- algo/lib/gcd.ex
+++ algo/lib/gcd.ex
@@ -1,6 +1,15 @@
 defmodule GCD do
+  # 对于a > b > 0 有复杂度为O(lg(b))
   def gcd(a, 0), do: a
   def gcd(a, b) do
     gcd(b, rem(a, b))
+  end
+
+
+  def egcd(a, 0), do: [{a, a, 0, 1, 0}]
+  def egcd(a, b) do
+    list = egcd(b, rem(a, b))
+    [{d, _a, _b, x, y} | _] = list
+    [{d, a, b, y, x - div(a, b) * y} | list]
   end
 end
--- algo/test/gcd_test.exs
+++ algo/test/gcd_test.exs
@@ -2,11 +2,17 @@ defmodule GCDTest do
   use ExUnit.Case, async: true
   import GCD
 
+
   test "gcd(a, b)" do
     assert gcd(6, 9) == 3
     assert gcd(7, 0) == 7
     assert gcd(0, 7) == 7
     assert gcd(3, 9) == 3
     assert gcd(12345, 4567890) == 15
+  end
+
+
+  test "egcd(a, b)" do
+    IO.inspect egcd(99, 78)
   end
 end
