88bcc6c7078da4ff3f0abe15076f74a996246f47
bencode <bencode@163.com>
2019-06-21 20:04:18 +0800

    use guards

--- exp/lib/exp.ex
+++
@@ -1,5 +0,0 @@
-defmodule Exp do
-  def main(_) do
-    IO.puts "hello world"
-  end
-end
---
+++ exp/lib/use_guards.ex
@@ -0,0 +1,32 @@
+defmodule UseGuards do
+  defguard is_even(n) when is_number(n) and rem(n, 2) == 0
+
+  def main() do
+    IO.inspect foo(123)
+    IO.inspect bar("345")
+  end
+
+  def foo(term) when is_integer(term) or is_float(term) or is_nil(term) do
+    :maybe_number
+  end
+
+  def foo(_other) do
+    :something_else
+  end
+
+  # 上面的条件可以简化成下面的语句
+  def bar(term)
+      when is_integer(term)
+      when is_float(term)
+      when is_nil(term) do
+    :maybe_number
+  end
+
+  def bar(_other) do
+    :something_else
+  end
+end
+
+
+
+UseGuards.main()
