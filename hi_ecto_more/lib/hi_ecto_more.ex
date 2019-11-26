1856a1895fdff2024bf54d2c1a56df12ca2c9823
bencode <bencode@163.com>
2019-11-26 15:59:35 +0800

    init

---
+++ hi_ecto_more/lib/hi_ecto_more.ex
@@ -0,0 +1,18 @@
+defmodule HiEctoMore do
+  @moduledoc """
+  Documentation for HiEctoMore.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HiEctoMore.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ hi_ecto_more/test/hi_ecto_more_test.exs
@@ -0,0 +1,8 @@
+defmodule HiEctoMoreTest do
+  use ExUnit.Case
+  doctest HiEctoMore
+
+  test "greets the world" do
+    assert HiEctoMore.hello() == :world
+  end
+end
