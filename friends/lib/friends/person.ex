2afa5b42253bec0d260da0094c3157793df0acbf
bencode <bencode@163.com>
2019-11-26 16:08:44 +0800

    remove friends

--- friends/lib/friends/person.ex
+++
@@ -1,9 +0,0 @@
-defmodule Friends.Person do
-  use Ecto.Schema
-
-  schema "people" do
-    field :first_name, :string
-    field :last_name, :string
-    field :age, :integer
-  end
-end
--- hi_ecto_more/test/hi_ecto_more_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HiEctoMoreTest do
-  use ExUnit.Case
-  doctest HiEctoMore
-
-  test "greets the world" do
-    assert HiEctoMore.hello() == :world
-  end
-end
