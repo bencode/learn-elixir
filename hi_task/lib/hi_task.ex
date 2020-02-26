632c732f82b264c84b5d53e4cbbdcf79b5c2a48a
bencode <bencode@163.com>
2020-02-26 17:29:33 +0800

    general use task

--- hi_task/lib/hi_task.ex
+++ hi_task/lib/hi_task.ex
@@ -2,20 +2,4 @@ defmodule HiTask do
   @moduledoc """
   use task
   """
-  require Logger
-
-  def general_use do
-    log_time("task 1", fn ->
-      task = Task.async(fn -> Process.sleep(100) end)
-      Task.await(task)
-    end)
-  end
-
-  defp log_time(name, fun) do
-    start = :os.system_time(:microsecond)
-    fun.()
-    finish = :os.system_time(:microsecond)
-    dur = finish - start
-    Logger.debug("#{name} cost #{dur}us}")
-  end
 end
--- hi_task/test/hi_task_test.exs
+++ hi_task/test/hi_task_test.exs
@@ -1,10 +1,24 @@
 defmodule HiTaskTest do
   use ExUnit.Case
   doctest HiTask
-
-  import HiTask
+  require Logger
 
   test "general use" do
-    general_use()
+    dur =
+      cost("task 1", fn ->
+        task = Task.async(fn -> Process.sleep(100) end)
+        Task.await(task)
+      end)
+
+    assert dur > 100 * 1000
+  end
+
+  defp cost(name, fun) do
+    start = :os.system_time(:microsecond)
+    fun.()
+    finish = :os.system_time(:microsecond)
+    dur = finish - start
+    Logger.debug("#{name} cost #{dur}us}")
+    dur
   end
 end
