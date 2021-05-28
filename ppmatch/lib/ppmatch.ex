37c00aba7d6b5ab00c72900d24f48f4b9264a84b
bencode <bencode@163.com>
2021-05-28 11:07:22 +0800

    add ppmatch

---
+++ ppmatch/lib/ppmatch.ex
@@ -0,0 +1,18 @@
+defmodule Ppmatch do
+  @moduledoc """
+  Documentation for `Ppmatch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ppmatch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ppmatch/test/ppmatch_test.exs
@@ -0,0 +1,8 @@
+defmodule PpmatchTest do
+  use ExUnit.Case
+  doctest Ppmatch
+
+  test "greets the world" do
+    assert Ppmatch.hello() == :world
+  end
+end
